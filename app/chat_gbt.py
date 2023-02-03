3e1645eb7c85e0b13639778279b998961d08c916
Krzysztof <k.konwinski@onet.pl>
2023-02-03 15:03:37 +0100

    return answer for questions

--- app/chat_gbt.py
+++ app/chat_gbt.py
@@ -1,21 +1,31 @@
 import openai
+import os
+from dotenv import load_dotenv
 
 
 class ChatGbtAdapter:
-    API_TOKEN = ''
+    load_dotenv()
 
-    def prepare_question(self, question: str):
-        openai.api_key = self.API_TOKEN
+    OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+
+    def prepare_response(self, question: str):
+        openai.api_key = self.OPENAI_API_KEY
         return openai.Completion.create(
             model="text-davinci-003",
             prompt=f"{question}",
-            temperature=0.9,
-            max_tokens=500,
+            temperature=0.7,
+            max_tokens=2000,
             top_p=1,
-            frequency_penalty=0.0,
-            presence_penalty=0.6,
-            stop=[" Human:", " AI:"]
+            frequency_penalty=1,
+            presence_penalty=1
         )
 
     def show_response(self, response: dict = None):
         print(response.get('choices')[0].get('text'))
+
+    def show_book_questions(self, found_author, found_book):
+        return {
+            'author': f'Opowiedz biografie autora: {found_author.name}',
+            'book': f'Napisz opracowanie "{found_book.title}"',
+            'epoch': f'Opowiedz coś o epoce {found_book.epoch}'
+        }
--- main.py
+++ main.py
@@ -16,6 +16,21 @@ class Application:
         books = adapter.get_books_details(author_books)
         for book in books:
             print(book)
+        search_book_id = int(input('Podaj numer książki, którą chcesz zobaczyć: '))
+        found_book = next(filter(lambda x: x.book_id == search_book_id, books))
+        print(f'Wybrałeś książkę: {found_book.title}')
+
+        chat_adapter = ChatGbtAdapter()
+        questions = chat_adapter.show_book_questions(found_author, found_book)
+        for question_id, question in enumerate(questions.values(), start=1):
+            print(f'{question_id}. {question}')
+        found_book_question = int(input('Wybierz pytanie: '))
+        question = list(questions.values())[found_book_question - 1]
+        if found_book_question == 2:
+            question = question + ' autora: ' + found_author.name
+
+        # response = chat_adapter.prepare_response(question)
+        # chat_adapter.show_response(response)
 
 
 if __name__ == '__main__':
